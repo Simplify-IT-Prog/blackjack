c3e6b513485a5daaf058eabeaa8b5db84ce677b7
R. Patel <simplify.it.prog@gmail.com>
2016-10-10 10:39:03 -0400

    first release

---
+++ Blackjack/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace CardGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which game shall we play:");
+            Console.WriteLine ("(B)lackjack?");
+
+            if (Console.ReadLine().ToUpper()=="B")
+            { //Code for Blackjack; can add other card games later.
+                //TODO add other card games
+                Play_Blackjack();
+                
+            }
+            
+            // Pause at end
+            Console.ReadLine();
+
+            //Console.WriteLine("{0}", newCard.ToString());
+            //DeckOfCards dealerDeck = new DeckOfCards(1);
+        }
+
+        public static void Play_Blackjack()
+        {
+            int numberCards_InitialDeal_Blackjack = 2;
+            int numberPlayers = 0;
+            string continuePlay;
+            
+            //Generate Shuffled deck
+            DeckOfCards shuffled_Deck = new DeckOfCards(1);
+
+            //Number of players
+            Console.Clear();
+            Console.Write("Enter the number of players: ");
+            Int32.TryParse(Console.ReadLine(), out numberPlayers);
+
+            //Initial Deal
+            //Assume last player is dealer.
+            CardHand[] playerHand = new CardHand[numberPlayers+1];
+            initialDeal(numberCards_InitialDeal_Blackjack, numberPlayers, playerHand, shuffled_Deck);
+
+            //For each player, display hand; only show first card of all opponents including dealer.
+            Console.Clear();
+            playerDecisions(playerHand, shuffled_Deck);
+            dealerDecisions(playerHand, shuffled_Deck);
+
+            DisplayGameStatus(playerHand);
+            Console.WriteLine("Would you like to play again? Y/N");
+            continuePlay = Console.ReadLine();
+            if (continuePlay.ToUpper() == "Y")
+                { Play_Blackjack(); }
+            else { System.Environment.Exit(0); }
+
+        }
+
+        public static void initialDeal(int numberCardsPerPlayer, int numberPlayers, CardHand[] player, DeckOfCards shuffled_Deck)
+        {//Deals two cards to each player in increasing order.  Also, records player's names during the process.
+            string name;
+            for (int j = 0; j < numberCardsPerPlayer; j++)
+            {
+                for (int i=0; i<= numberPlayers; i++)
+                {
+                    if (j == 0)
+                    {
+                        if (i != numberPlayers)
+                        {
+                            Console.WriteLine("Enter Player {0}'s name:", i+1);
+                            name = Console.ReadLine ();
+                        } else 
+                        {
+                            name = "Dealer";
+                        }
+                        player[i] = new CardHand(name);
+                    }
+                    player[i].TakeCard(shuffled_Deck.DealCard());
+                }
+            }
+        }
+
+        public static void playerDecisions (CardHand[] player, DeckOfCards shuffled_Deck)
+        {
+            for (int i = 0; i < player.GetUpperBound(0); i++) //The decision rests on i; no decisions for dealer.
+            {//Print opponent's hands.
+                Console.Clear();
+                for (int j = 0; j <= player.GetUpperBound(0); j++)
+                {
+                    if (j != i)
+                    {
+                        writePlayerHand(j, false, player);
+                    }
+                }
+
+                //Go back and print i
+                Console.WriteLine();
+                writePlayerHand(i, true, player);
+
+                //Hit or Stay?
+                Console.WriteLine();
+                string hitChoice;
+                do
+                {
+                    Console.WriteLine("Your current score: {0}.", player[i].score);
+                    Console.Write($"{player[i].playerName}, (H)it or (S)tay: ");
+                    hitChoice = Console.ReadLine().ToUpper();
+                    if (hitChoice == "H")
+                    {
+                        player[i].TakeCard(shuffled_Deck.DealCard());
+                        writePlayerHand(i, true, player);
+                        WinnerStatus(player[i].score, player[player.GetUpperBound(0)].score, false);
+                        if (player[i].score>=21) { break; }  //Exit if Blackjack or Bust
+                    }
+                } while (hitChoice.ToUpper() == "H");
+                continue;
+            }
+        }
+        public static void dealerDecisions(CardHand[] player, DeckOfCards shuffled_Deck)
+        {// Automate dealer's decisions to hit or stay.
+            while (player[player.GetUpperBound(0)].score < 16)
+            {
+                player[player.GetUpperBound(0)].TakeCard(shuffled_Deck.DealCard());
+            }
+        }
+        public static void WinnerStatus(int score, int dealerScore, bool finalStatus)
+        {
+            if (score == 21)
+            {
+                Console.WriteLine("Blackjack!  You're a WINNER!");
+            } else if(score > 21)
+            {
+                Console.WriteLine("Bust!  try again!");
+            }
+            if (finalStatus == true)
+            {
+                if (score < 21 && dealerScore < 21)
+                {
+                    Console.WriteLine((score > dealerScore) ? ("WINNER! You beat the dealer's hand.") : ("LOST! The dealer's hand beats yours."));
+                } else if (score > 21 && dealerScore > 21)
+                {
+                    Console.WriteLine("Draw!");
+                }
+            }
+        }
+        public static void writePlayerHand(int printIndex, bool boolDisplayAllCards, CardHand[] player)
+        { //Prints opponents hands and then prints player's hand
+            Console.WriteLine($"{player[printIndex].playerName}'s hand:");
+            if (boolDisplayAllCards == false)
+            { //Opponent's hand: show only first card
+                Console.WriteLine ("   {0}", player[printIndex].hand[0].ToString());
+                for (int i = 1; i <= player[printIndex].hand.GetUpperBound(0); i++)
+                {
+                    Console.WriteLine("   * - Face Down");
+                }
+                
+            } else
+            { // Show all cards.
+                for (int i = 0; i <= player[printIndex].hand.GetUpperBound(0); i++)
+                {
+                    Console.WriteLine("   {0}", player[printIndex].hand[i].ToString());
+                }
+            }
+        }
+        public static void DisplayGameStatus(CardHand[] player)
+        {
+            Console.Clear();
+            for (int j = 0; j <= player.GetUpperBound(0); j++)
+            {// Display each player's hand and then their game status.
+                writePlayerHand(j, true, player);
+                WinnerStatus(player[j].score, player[player.GetUpperBound(0)].score, true);
+            }
+        }
+    }
+}
---
+++ Blackjack/cards.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace CardGames
+{
+    public enum Suit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+    public enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+    public struct Card
+    {
+        public Suit cardSuit { get; private set; }
+        public Rank cardRank { get; private set; }
+
+        public Card(Suit s, Rank r)
+        { // One card with a specific Suit and Rank.
+            cardSuit = s;
+            cardRank = r;
+        }
+        public override string ToString()
+        {
+            return cardRank.ToString() + " of " + cardSuit.ToString();
+        }
+        public static string DisplaySuit(Card x)
+        {
+            Enum cardSuit = x.cardSuit;
+            return cardSuit.ToString();
+        }
+        public static string DisplayRank(Card x)
+        {
+            Enum cardRank = x.cardRank;
+            return cardRank.ToString();
+        }
+
+    }
+    public class DeckOfCards
+    {
+        private Card[] deck;
+        private int cardsDealt = 0;
+        int numCardsPerDeck = 52; // May someday expand to include Jokers.
+
+        public DeckOfCards(int numberOfDecks)
+        { // Generates a shuffled deck of cards based on the desired number of decks.
+            deck = new Card[numCardsPerDeck * numberOfDecks];
+
+            for (int i = 0; i<numberOfDecks; i++)
+            {
+                foreach (int j in Enum.GetValues(typeof(Suit)))
+                {
+                    foreach (int k in Enum.GetValues(typeof(Rank)))
+                    { // Assigns a card to each index of the deck: ordered based on listing of Suit and Rank in Enums.
+                        deck[(i * (numCardsPerDeck)) + (j * (Enum.GetValues(typeof(Rank)).Length)) + k] = new Card((Suit)j, (Rank)k);
+                    }
+                }
+            }
+            Shuffle();
+        }
+
+        private void Shuffle()
+        { // Shuffle entire deck.
+            Random rnd = new Random();
+            for (int i = (deck.Length-1); i>=0; i--)
+            {
+                SwapTwoCards(rnd.Next(i), i);
+            }
+        }
+        private void SwapTwoCards(int index1, int index2)
+        { // Swap the two cards at the submitted indexes from array deck.
+            Card tempCard = deck[index2];
+            deck[index2] = deck[index1];
+            deck[index1] = tempCard;
+        }
+
+        public Card DealCard()
+        { // deal first card from the shuffled deck. 
+
+            //check to see if there are cards in the deck that can be dealt to players.  If not, shuffled deck, then issue card.
+            if (cardsDealt > deck.GetUpperBound(0))
+            {
+                Shuffle();
+                cardsDealt = 0;
+                return deck[cardsDealt++];
+            }
+            else { return deck[cardsDealt++]; }
+        }
+    }
+    public class CardHand
+    { // A player's hand
+        public Card[] hand { get; private set; }
+        public string playerName { get; private set; }
+        public int score { get; private set; }
+
+        // Constructor for new hand
+        public CardHand(string name)
+        {
+            score = 0;
+            playerName = name;
+        }
+
+        // Ask dealer for card and save in playerhand
+        public void TakeCard(Card dealtCard)
+        {
+            if (hand == null )
+            {
+                hand = new Card[1];
+                hand[0] = dealtCard;
+            } else
+            {
+                Card[] tempHand = hand;
+                hand = new Card[tempHand.Length+1];
+                tempHand.CopyTo(hand, 0);
+                hand[hand.GetUpperBound(0)] = dealtCard;
+            }
+            DetermineScore();
+        }
+
+        // Determine player score for Blackjack
+        private void DetermineScore()
+        {
+            score = 0;
+            // Loop through cards and sum scores
+            foreach (Card cardInHand in hand)
+            {
+                switch (cardInHand.cardRank)
+                {
+                    case Rank.Ace:
+                        score += 11;
+                        break;
+                    case Rank.Two:
+                        score += 2;
+                        break;
+                    case Rank.Three:
+                        score += 3;
+                        break;
+                    case  Rank.Four:
+                        score += 4;
+                        break;
+                    case Rank.Five:
+                        score += 5;
+                        break;
+                    case Rank.Six:
+                        score += 6;
+                        break;
+                    case Rank.Seven:
+                        score += 7;
+                        break;
+                    case Rank.Eight:
+                        score += 8;
+                        break;
+                    case Rank.Nine:
+                        score += 9;
+                        break;
+                    case Rank.Ten:
+                    case Rank.Jack:
+                    case Rank.Queen:
+                    case Rank.King:
+                        score += 10;
+                        break;
+                    default:
+                        break;
+                } 
+            }
+        }
+
+    }
+
+
+}
+
+
